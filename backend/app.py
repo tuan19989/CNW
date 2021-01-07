43e48f9adc803eff162dc947f4dedb04a9fbd515
pam Cong Tuan <phamcongtuan222333@gmail.com>
2021-01-07 16:15:55 +0700

    fix

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ ConnectionData['database'] = 'northwind'
 
 @app.route("/")
 def hello():
-   return "hello"
+   return "hello213"
 @app.route("/test_insert")
 def test_insert():
     c2 = do.Customer(ConnectionData)
