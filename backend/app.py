d2b57b1b351aab6fdb9c3cd081eaa85a7b4e5bb8
pam Cong Tuan <phamcongtuan222333@gmail.com>
2021-01-26 18:11:20 +0700

    gi

--- backend/app.py
+++ backend/app.py
@@ -19,8 +19,8 @@ def hello():
    return "hello1234"
 @app.route("/test_insert")
 def test_insert():
-    c2 = do.Employee(ConnectionData)
-    c1 = bo.Employee('Tuan','Pham','02051998','','VIET NAM')
+    c2 = do.OrderDetail(ConnectionData)
+    c1 = bo.OrderDetail(1,'Tuan','Pham')
     s1 = c2.insert(c1)
     return s1
 
