bcdd02728d86da447e98ab440d70f84a5afb040d
pam Cong Tuan <phamcongtuan222333@gmail.com>
2021-01-26 18:27:51 +0700

    fix

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ def hello():
 @app.route("/test_insert")
 def test_insert():
     c2 = do.OrderDetail(ConnectionData)
-    c1 = bo.OrderDetail('1','Tuan','Pham')
+    c1 = bo.OrderDetail('vipvip','Tuan','Pham')
     s1 = c2.insert(c1)
     return s1
 
