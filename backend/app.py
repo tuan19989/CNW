eaef7debd109b520bbd7290e236ca9d43f409c56
pam Cong Tuan <phamcongtuan222333@gmail.com>
2021-01-27 17:17:44 +0700

    fix

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ def hello():
 @app.route("/test_insert")
 def test_insert():
     c2 = do.Employee(ConnectionData)
-    c1 = bo.Employee(1','Pham','Tuan','1998','Quang Nam','Viet Nam')
+    c1 = bo.Employee(1,'Pham','Tuan','1998','Quang Nam','Viet Nam')
     s1 = c2.insert(c1)
     return s1
 
